b44370ff85a5d6e7ccec9c55957d2095d48f52fc
Rohit Gulam <rohitsgulam@gmail.com>
2023-10-01 10:09:35 +0300

    arranged folders for good organization

--- Apple Frameworks/Screen/FFrameworkGridView/FrameworkGridView.swift
+++ Apple Frameworks/Screen/FFrameworkGridView/FrameworkGridView.swift
@@ -11,14 +11,11 @@ struct FrameworkGridView: View {
     
     @StateObject var viewModel = FrameworkGridViewModel()
     
-    let columns: [GridItem] = [GridItem(.flexible()),
-                               GridItem(.flexible()),
-                               GridItem(.flexible())]
     
     var body: some View {
         NavigationView{
             ScrollView{
-                LazyVGrid(columns: columns){
+                LazyVGrid(columns: viewModel.columns){
                     ForEach(MockData.frameworks) { framework in
                         FrameworkTitleView(framework: framework)
                             .onTapGesture {
@@ -28,7 +25,7 @@ struct FrameworkGridView: View {
                 }
             }
             .navigationTitle("🍎 Frameworks")
-            .sheet(isPresented: $viewModel.isShowingDetailView) {
+            .fullScreenCover(isPresented: $viewModel.isShowingDetailView) {
                 FrameworkDetailView(framework: viewModel.selectedFramework ?? MockData.sampleFramework, isShowingDetailView: $viewModel.isShowingDetailView)
             }
         }
@@ -39,24 +36,5 @@ struct FrameworkGridView: View {
 struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
-    }
-}
-
-struct FrameworkTitleView: View {
-    
-    let framework: Framework
-    
-    var body: some View{
-        VStack{
-            Image(framework.imageName)
-                .resizable()
-                .frame(width: 90, height: 90)
-            Text(framework.name)
-                .font(.title2)
-                .fontWeight(.semibold)
-                .scaledToFit()
-                .minimumScaleFactor(0.6)
-        }
-        .padding()
     }
 }
