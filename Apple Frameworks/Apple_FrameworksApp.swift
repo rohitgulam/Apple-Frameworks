1772a95df04fa1e88d1219433236160ffba1633c
Rohit Gulam <rohitsgulam@gmail.com>
2023-10-01 06:04:37 +0300

    Initial Commit

---
+++ Apple Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple Frameworks
+//
+//  Created by Rohit Gulam on 01/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
